b41afd2e8bce6e4656e6eb51751ef177279b834c
trideep.k <trideep.k@talview.com>
2019-02-27 10:17:27 +0530

    missing icon added

--- src/screens/HomeScreen/index.js
+++ src/screens/HomeScreen/index.js
@@ -1,6 +1,6 @@
 import React, { Component } from 'React';
 import { View, Text, StyleSheet, Button, TouchableOpacity } from 'react-native';
-import Icon from 'react-native-vector-icons/FontAwesome5';
+import Icon from 'react-native-vector-icons/Ionicons';
 
 export default class HomeScreen extends Component {
   static navigationOptions = ({ navigation }) => {
@@ -9,7 +9,7 @@ export default class HomeScreen extends Component {
       headerRight: (
         <View style={{margin: 8}}>
           <TouchableOpacity onPress={() => navigation.toggleDrawer()}>
-            <Icon size={25} name="fa-bars" color="white"></Icon>
+            <Icon name="ios-menu" size={28} color="#fff" />
           </TouchableOpacity>
         </View>
       ),
--- src/screens/MovieScreens/index.js
+++ src/screens/MovieScreens/index.js
@@ -1,6 +1,6 @@
 import React, { Component } from 'React';
 import { View, Text, Button, TouchableOpacity, Image } from 'react-native';
-import Icon from 'react-native-vector-icons/FontAwesome5';
+import Icon from 'react-native-vector-icons/Ionicons';
 
 export default class MovieScreen extends Component {
   static navigationOptions = ({ navigation }) => {
@@ -9,7 +9,7 @@ export default class MovieScreen extends Component {
       headerRight: (
         <View style={{margin: 8}}>
           <TouchableOpacity onPress={() => navigation.toggleDrawer()}>
-            <Icon name="fa-bars" size={25} color="#fff" />
+            <Icon name="ios-menu" size={28} color="#fff" />
           </TouchableOpacity>
         </View>
       ),
